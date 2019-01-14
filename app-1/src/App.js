b37de48973aeabb669cff624de6833c6162db012
David Wells <davidwells@Davids-MacBook-Pro.local>
2019-01-14 15:44:44 -0700

    finsished first drill

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.state= {
+      userInput: ""
+    }
+  }
+  updateInput(value)
+  {
+    this.setState({
+      userInput: value
+    });
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input
+      onChange={(event) => this.updateInput(event.target.value)}/>
+      <h4>
+        {this.state.userInput}
+      </h4>
       </div>
     );
   }
